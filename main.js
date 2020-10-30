496ffd1b770504476c0811a71bc33d760f84c38a
James Stanley <james@incoherency.co.uk>
2020-10-30 23:39:27 +0000

    Add index.html, more work

--- main.js
+++ main.js
@@ -4,17 +4,30 @@ const express = require('express');
 const fs = require('fs');
 
 let seasonal_css = '';
+let index_html = '';
 
 const app = express();
+app.get('/', (req,res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.send(index_html);
+});
 app.get('/seasonal.css', (req,res) => {
     res.setHeader('Content-Type', 'text/css');
+    res.setHeader('Cache-Control', 'nocache');
+    res.setHeader('Expires', 'now');
     res.send(generate_css(req.query.day));
 });
 
+// XXX: race condition while loading up where we might get a request before data is loaded
 fs.readFile('seasonal.css', 'utf8', function (err,data) {
     if (err)
         return console.log(err);
     seasonal_css = data;
+});
+fs.readFile('index.html', 'utf8', function (err,data) {
+    if (err)
+        return console.log(err);
+    index_html = data;
 });
 
 // https://stackoverflow.com/a/8619946
@@ -53,18 +66,18 @@ function blend(hsl1, hsl2, k) {
 // https://v6.robweychert.com/blog/2019/12/dynamic-color-javascript-hsl/
 let keyframes = [
   //  day    hue     sat      hlsat
-  [   0,     270,    0.10,    1.00  ],
+  [   0,     270,    0.08,    0.50  ],
   [  31,     240,    0.11,    1.00  ],
-  [  60,     210,    0.20,    0.85  ],
-  [  91,     180,    0.22,    0.75  ],
-  [ 120,     150,    0.26,    1.00  ],
-  [ 151,     120,    0.20,    1.00  ],
-  [ 181,      90,    0.28,    1.00  ],
-  [ 212,      60,    0.28,    0.70  ],
-  [ 243,      30,    0.32,    0.85  ],
-  [ 273,       0,    0.18,    0.75  ],
-  [ 304,     -30,    0.14,    0.75  ],
-  [ 334,     -60,    0.10,    0.60  ],
+  [  60,     210,    0.20,    0.55  ],
+  [  91,     180,    0.22,    0.55  ],
+  [ 120,     150,    0.26,    0.55  ],
+  [ 151,     120,    0.20,    0.55  ],
+  [ 181,      90,    0.40,    0.55  ],
+  [ 212,      60,    0.28,    0.55  ],
+  [ 243,      30,    0.32,    0.70  ],
+  [ 273,       0,    0.18,    0.45  ],
+  [ 304,     -30,    0.10,    0.35  ],
+  [ 334,     -60,    0.10,    1.00  ],
   [ 365,     -90,    0.10,    1.00  ]
 ];
 
@@ -85,15 +98,13 @@ function generate_css(requested_day) {
         day = requested_day;
     }
 
-    //day = 210;
-
     let hue, hlhue, sat, hlsat;
     for (let i = 1; i < keyframes.length; i++) {
         if (keyframes[i][0] >= day) {
             let k = (day - keyframes[i-1][0]) / (keyframes[i][0] - keyframes[i-1][0]);
             hue = lerp(keyframes[i-1][1], keyframes[i][1], k);
-            hlhue = hue + 120;
-            sat = 150*lerp(keyframes[i-1][2], keyframes[i][2], k);
+            hlhue = hue - 30;
+            sat = 100*lerp(keyframes[i-1][2], keyframes[i][2], k);
             hlsat = 100*lerp(keyframes[i-1][3], keyframes[i][3], k);
             break;
         }
@@ -101,8 +112,10 @@ function generate_css(requested_day) {
 
     let vars = {
         bg: [hue,sat,96],//blend(season1.bg, season2.bg, proportion),
+        bgdark: [hue,sat,90],//blend(season1.bg, season2.bg, proportion),
         fg: [hue,sat,30],//blend(season1.fg, season2.fg, proportion),
         hl: [hlhue,hlsat,50],//blend(season1.hl, season2.hl, proportion),
+        hldark: [hlhue, hlsat, 20],
     };
 
     let css = "/* Seasonal.css by James Stanley */\n";
