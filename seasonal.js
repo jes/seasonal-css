16b2fc44f0ce36e4b3d90d95cc3b214dce602c98
James Stanley <james@incoherency.co.uk>
2020-11-01 18:55:32 +0000

    Set favicon in javascript, fix off-by-1 on date calculation, make some colours easier to read

--- seasonal.js
+++ seasonal.js
@@ -4,12 +4,12 @@
       //  day    hue     sat      hlsat
       [   0,     270,    0.08,    0.30  ],
       [  31,     240,    0.11,    0.40  ],
-      [  60,     210,    0.20,    0.45  ],
+      [  60,     210,    0.20,    0.40  ],
       [  91,     180,    0.20,    0.40  ],
       [ 120,     150,    0.18,    0.40  ],
       [ 151,     120,    0.15,    0.40  ],
-      [ 181,      90,    0.18,    0.30  ],
-      [ 212,      60,    0.21,    0.45  ],
+      [ 181,      90,    0.18,    0.20  ],
+      [ 212,      60,    0.21,    0.35  ],
       [ 243,      30,    0.28,    0.60  ],
       [ 273,       0,    0.18,    0.45  ],
       [ 304,     -30,    0.10,    0.25  ],
@@ -26,13 +26,13 @@
     // https://stackoverflow.com/a/8619946
     exports.day_of_year = function() {
         var now = new Date();
-        var start = new Date(now.getFullYear(), 0, 0);
+        var start = new Date(now.getFullYear(), 0, 1);
         var diff = now - start;
         var oneDay = 1000 * 60 * 60 * 24;
         return Math.floor(diff / oneDay);
     }
 
-    exports.css = function(requested_day, varsonly) {
+    exports.vars = function(requested_day) {
         let day = exports.day_of_year();
 
         if (requested_day !== undefined && requested_day >= 0 && requested_day < 366)
@@ -50,19 +50,23 @@
             }
         }
 
-        let vars = {
-            bg: [hue,sat,96],
-            bgdark: [hue,sat,90],
-            fg: [hue,sat,30],
-            hl: [hlhue,hlsat,50],
-            hldark: [hlhue, hlsat, 35],
+        return {
+            bg: 'hsl(' + hue + ',' + sat + '%,96%)',
+            bgdark: 'hsl(' + hue + ',' + sat + '%,90%)',
+            fg: 'hsl(' + hue + ',' + sat + '%,30%)',
+            hl: 'hsl(' + hlhue + ',' + hlsat + '%,50%)',
+            hldark: 'hsl(' + hlhue + ',' + hlsat + '%,35%)'
         };
+    }
+
+    exports.css = function(requested_day) {
+        let vars = exports.vars(requested_day);
 
         let css = "/* Seasonal.css by James Stanley\n   https://seasonal-css.incoherency.co.uk/ */\n";
         css += ":root {\n";
         for (let varname in vars) {
             let col = vars[varname];
-            css += "    --seasonal-" + varname + ": hsl(" + col[0] + "," + col[1] + "%," + col[2] + "%);\n";
+            css += "    --seasonal-" + varname + ": " + vars[varname] + ";\n";
         }
         css += "}\n\n";
 
